89d664d52a9b8850c7183479095136836876c6a4
Poohowy <grabowski14@hotmail.com>
2018-05-20 17:00:50 +0200

    Change Jenkinsfile

--- src/main/scala/ScalaObject.scala
+++ src/main/scala/ScalaObject.scala
@@ -7,8 +7,6 @@ class ScalaObject {
 object ScalaObject {
   def main(args: Array[String]) {
     val scalaObject = new ScalaObject()
-    val javaObject = new JavaObject()
     scalaObject.sayHello()
-    javaObject.sayHello()
   }
 }
